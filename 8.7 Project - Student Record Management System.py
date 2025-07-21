d3b14be51394b094ce807ffdc4fb7719126ef69d
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2025-07-21 19:38:07 +0100

    Create tuples to store student information (name, age, grade).

---
+++ 8.7 Project - Student Record Management System.py
@@ -0,0 +1,14 @@
+#Project: Student Record Management System
+
+# 1. Create tuples to store student information (name, age, grade).
+student_name = ("Christina", "Bobby", "Danniel")
+student_age = ("31", "28", "24")
+student_grade = ("A", "C", "B")
+
+# 2. Use tuple methods to count and index elements.
+
+# 3. Create sets to store unique student IDs and courses.
+
+# 4. Perform set operations like union, intersection, and difference.
+
+# 5. Use frozen sets to create immutable sets of student data.
