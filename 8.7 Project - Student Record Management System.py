ca4d27a93d8cf561229b059af17cf642a0e56bbd
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2025-07-21 19:59:57 +0100

    Create sets to store unique student IDs and courses.

--- 8.7 Project - Student Record Management System.py
+++ 8.7 Project - Student Record Management System.py
@@ -10,6 +10,8 @@ grade_count = student_grades.count("B")
 print(f"This is {student_names[1]} aged {student_ages[1]}. Their test result was a B.")
 
 # 3. Create sets to store unique student IDs and courses.
+student_id = (1002, 1003, 1004, 1005, 1006, 1007)
+student_courses =("Web Programming", "Social Media", "Web Programming", "English", "Web Programming")
 
 # 4. Perform set operations like union, intersection, and difference.
 
